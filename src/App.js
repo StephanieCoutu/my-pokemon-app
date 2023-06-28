25d1f1f0c272d5d2e8881e490f8754e922a942aa
Stéphanie Coutu <steph_9_4@hotmail.com>
2023-06-27 21:23:24 -0400

    Erreur dans mon fichier d'origine - j'ai refait

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const pokemonData = [
+    {
+      name: 'Archeduc',
+      image: 'archeduc.png',
+      abilities: [  
+      "Brasénergie",
+      "Danse-Plume",
+      "Lame d'Air",
+    ],
+    },
+    {
+      name: 'Brindibou',
+      image: 'Brindibou.png',
+      abilities: [
+      "Lance-Flamme",
+      "Éco-Sphère",
+      "Hurlement",
+    ],
+    },
+    {
+      name: 'Cerbyllin',
+      image: 'cerbyllin.png',
+      abilities: [
+      "Boule de Feu",
+      "Feu Follet",
+      "Fouet Lianes",
+    ],
+    },
+    {
+      name: 'Clamiral',
+      image: 'clamiral.png',
+      abilities: [
+        "Hydrocanon",
+        "Laser Glace",
+        "Blizzard",
+      ],
+    },
+    {
+      name: 'Effleche',
+      image: 'effleche.png',
+      abilities: [
+        "Bec Vrille",
+        "Tranch'Herbe",
+        "Aéropique",
+      ],
+    },
+    {
+      name: 'Feurisson',
+      image: 'feurisson.png',
+      abilities: [
+        "Roue de Feu",
+        "Lance-Flammes",
+        "Vitesse Extrême",
+      ],
+    },
+    {
+      name: 'Mateloutre',
+      image: 'mateloutre.png',
+      abilities: [
+        "Aqua-Jet",
+        "Hydrocanon",
+        "Vibraqua",
+      ],
+    },
+    {
+      name: 'Moustillon',
+      image: 'moustillon.png',
+      abilities: [
+        "Pistolet à O",
+        "Bulles d'O",
+        "Griffe",
+      ],
+    },
+    {
+      name: 'Paragruel',
+      image: 'paragruel.png',
+      abilities: [
+        "Hydrocanon",
+        "Vibraqua",
+        "Éboulement",
+      ],
+    },
+    {
+      name: 'Typhlosion',
+      image: 'typhlosion.png',
+      abilities: [
+        "Lance-Flamme",
+        "Éruption",
+        "Roue de Feu",
+      ],
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemonData.map((pokemon, index) => (
+        <PokemonCard
+          key={index}
+          name={pokemon.name}
+          image={pokemon.image}
+          abilities={pokemon.abilities}
+        />
+      ))}
     </div>
   );
-}
+};
+
+const PokemonCard = ({ name, image, abilities }) => {
+  const [showAbilities, setShowAbilities] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowAbilities(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowAbilities(false);
+  };
+
+  return (
+    <div
+      className="pokemon-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img src={process.env.PUBLIC_URL + image} alt={name} />
+      {showAbilities && (
+        <div className="abilities">
+          {abilities.map((ability, index) => (
+            <p key={index}>{ability}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
+
